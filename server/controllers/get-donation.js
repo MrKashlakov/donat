4a5e10c1bb9a1b3c032de776c519916a09b17a7c
Ilya Kashlakov <kashlakov@yamoney.ru>
2017-06-10 17:30:21 +0300

    WIP

--- server/controllers/get-donation.js
+++ server/controllers/get-donation.js
@@ -9,7 +9,15 @@ class GetDonationController {
     if (label) {
       mongoose.connect('mongodb://localhost/donat');
       const Donation = mongoose.model('Donation', DonationSchema);
-      Donation.find({ id: label }, (donation) => {
+      Donation.find({ id: label }, (err, donation) => {
+        if (err) {
+          throw err;
+        }
+
+        if (donation === null) {
+          return;
+        }
+
         socket.emit('donation alert', { sum: amount, username: donation.username, message: donation.message });
       });
     }
