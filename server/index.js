832279c976e36b67e91954ae9eda4c89c79f8853
Ilya Kashlakov <kashlakov@yamoney.ru>
2017-06-10 14:47:13 +0300

    WIP

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ const isDev = process.env.NODE_ENV !== 'production';
 const ngrok = (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel ? require('ngrok') : false;
 const resolve = require('path').resolve;
 const app = express();
+const io = require('socket.io')(app);
 
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
 // app.use('/api', myApi);
@@ -16,7 +17,7 @@ const app = express();
 // In production we need to pass these values in instead of relying on webpack
 setup(app, {
   outputPath: resolve(process.cwd(), 'build'),
-  publicPath: '/'
+  publicPath: '/',
 });
 
 // get the intended host and port number, use localhost and port 3000 if not provided
@@ -25,6 +26,10 @@ const host = customHost || null; // Let http.Server use its default IPv6/4 host
 const prettyHost = customHost || 'localhost';
 
 const port = argv.port || process.env.PORT || 3000;
+
+io.on('connection', () => {
+  io.emit('donation alet', { sum: '100', username: '@test', message: 'Hi! Donate 100 for coding' });
+});
 
 // Start your app.
 app.listen(port, host, (err) => {
